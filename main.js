c2174b24cebb81d38b46c71007f44c38fb32a4c3
rev_matcher <gkaustav57@gmail.com>
2020-08-22 09:43:31 +0530

    added index.html and styles.css

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+var budgetController =(function add(){
+    const x = 23;
+
+
+
+})();
+
+var UIcontroller =(function(){
+
+
+    //some code
+})();
+
+var controller =(function(){
+
+ 
+})(budgetController,UIcontroller);
